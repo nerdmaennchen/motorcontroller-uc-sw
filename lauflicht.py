ff328f66ed31b5433ff2c0689ff69ece98802b56
Lutz <lutz.freitag@fu-berlin.de>
2017-01-18 15:16:07 +0100

    updated the blingbling code to the adjusted interface

--- lauflicht.py
+++ lauflicht.py
@@ -4,19 +4,33 @@ import time
 import random
 
 def flush(vals):
-	setConfig(dev, configs, "led.values", tuple(vals), False)
+	setConfig(dev, configs, "led.values.front", tuple(vals), False)
+	setConfig(dev, configs, "led.values.back",  tuple(vals + [0]), False)
 
 def fade():
 	while True:
 		for brightness in range(0, 255):
-			vals = [brightness << 16 | brightness << 8 | brightness] * numLEDs
+			vals = [brightness << 16 | brightness << 8 | 0] * numLEDs
 			flush(vals)
-			time.sleep(0.005)
+			time.sleep(0.00001)
 		for brightness in range(255, 0, -1):
-			vals = [brightness << 16 | brightness << 8 | brightness] * numLEDs
+			vals = [brightness << 16 | brightness << 8 | 0] * numLEDs
 			flush(vals)
-			time.sleep(0.005)
-
+			time.sleep(0.00001)
+			
+def knightRider():
+	numLEDsFront = int(configs["led.values.front"][1] / 4)
+	numLEDsBack  = int(configs["led.values.back"][1] / 4)
+	while True:
+		for i in list(range(0, numLEDsFront, 1)) + list(range(numLEDsFront-1, -1, -1)):
+			valsFront = [0] * numLEDsFront
+			valsFront[i] = 255 << 8
+			setConfig(dev, configs, "led.values.front", tuple(valsFront), False)
+			valsBack = [0] * numLEDsBack
+			valsBack[i] = 255 << 8
+			setConfig(dev, configs, "led.values.back",  tuple(valsBack), False)
+			time.sleep(0.025)
+			
 def randomStuff():
 	random.seed()
 	ledArr = [0] * numLEDs
@@ -26,11 +40,11 @@ def randomStuff():
 		if idx >= numLEDs:
 			idx = 0
 		ledArr[idx] = random.randint(0, 0xffffff) #0xff0000
-		setConfig(dev, configs, "led.values", tuple(ledArr), False)
+		setConfig(dev, configs, "led.values.front", tuple(ledArr), False)
 		time.sleep(.01)
 	
 def lauflicht():
-	numLEDs = int(configs["led.values"][1] / 4)
+	numLEDs = int(configs["led.values.front"][1] / 4)
 	ledArr = [0] * numLEDs
 	idx = 0
 	while True:
@@ -39,7 +53,7 @@ def lauflicht():
 		if idx >= numLEDs:
 			idx = 0
 		ledArr[idx] = 0xff0000
-		setConfig(dev, configs, "led.values", tuple(ledArr), False)
+		setConfig(dev, configs, "led.values.front", tuple(ledArr), False)
 		time.sleep(.01)
 
 
@@ -50,7 +64,9 @@ if __name__ == "__main__":
 
 	dev.set_configuration()
 	configs = fetchConfig(dev)
-	numLEDs = int(configs["led.values"][1] / 4)
-	
+	numLEDs = int(configs["led.values.front"][1] / 4)
+
+	knightRider()
 	fade()
+	randomStuff()
 
